57502e9c6b303b2813b456bd2d7990cfc9d481a7
lijiang <tracey9s@163.com>
2026-05-23 16:13:23 +0800

    feat:公告取的高度也要随内容变化

--- src/components/toolbox/NoticePanel.vue
+++ src/components/toolbox/NoticePanel.vue
@@ -5,12 +5,12 @@
     <div class="notice-content">
       <p>软件在使用中，如有建议或 BUG 反馈，请添加下方的 QQ 号反馈。</p>
       <p>后期不断更新，优化和增加功能。</p>
-      <p class="notice-hot">右键点击按钮，可查看功能介绍！</p>
+      <!-- <p class="notice-hot">右键点击按钮，可查看功能介绍！</p> -->
     </div>
 
-    <div class="recommend-card">
+    <!-- <div class="recommend-card">
       <span class="recommend-label">强烈推荐</span>
       <strong>卸载流氓软件（360、腾讯管家、金山杀毒、2345 卫士），安装“火绒安全”免费版！</strong>
-    </div>
+    </div> -->
   </section>
 </template>
